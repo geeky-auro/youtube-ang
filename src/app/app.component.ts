2a547b010e798e89bd315ae28668abd7e0462d81
Auro Saswat Raj <aurosaswat.apk@gmail.com>
2023-06-17 02:27:38 +0530

    updated app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,9 +11,13 @@ import { TODO } from "./todo";
 })
 export class AppComponent {
   title = 'Project CC';
-  
-  todoValue:string="";
-  list:TODO[]=[];
+  todoValue:string;
+  list:TODO[];
+
+  constructor(){
+    this.todoValue="";
+    this.list=[];
+  }
 
 
   ngOnInit(){
@@ -34,6 +38,19 @@ export class AppComponent {
     this.todoValue="";
   }
 
+/**
+ * 
+ * Within the callback function, item refers to each individual item in the list array. The expression item.id!==id compares the id property of each item with the id parameter passed to the deleteItem() method.
+
+If the id of the current item is not equal to the id parameter, the callback function returns true, indicating that the item should be kept in the filtered array.
+
+If the id of the current item is equal to the id parameter, the callback function returns false, indicating that the item should be excluded from the filtered array.
+
+The filter() method creates a new array with all the items that passed the condition (item.id!==id), effectively removing the item with the matching id from the list array.
+
+Finally, the list array is updated with the filtered array, effectively removing the item from the original array.
+ */
+  
   deleteItem(id:number){
     this.list=this.list.filter(item => item.id!==id);
   }
