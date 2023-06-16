83511c5e8f1a3ce206a5050bec7be030fd8ae5d0
Auro Saswat Raj <aurosaswat.apk@gmail.com>
2023-06-17 01:54:14 +0530

    TODO list Initialized ;)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,40 @@
 import { Component } from '@angular/core';
 
+import { TODO } from "./todo";
+
+
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'youtube-ang';
+  title = 'Project CC';
+  
+  todoValue:string="";
+  list:TODO[]=[];
+
+
+  ngOnInit(){
+    this.list=[];
+    this.todoValue="";
+  }
+
+  addItem(){
+    if (this.todoValue!=="") {
+      const newItem:TODO={
+        id:Date.now(),
+        value:this.todoValue,
+        isDone:false
+      };
+      this.list.push(newItem);
+
+    }
+    this.todoValue="";
+  }
+
+  deleteItem(id:number){
+    this.list=this.list.filter(item => item.id!==id);
+  }
 }
